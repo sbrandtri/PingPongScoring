b0a605b5d74913553bb072e5b4325dacd10eeb90
Shawn Brandt <shawnri@gmail.com>
2021-04-29 23:16:56 -0400

    Update karma.conf.js to match new project

--- ping-pong-scoring-app/karma.conf.js
+++ ping-pong-scoring-app/karma.conf.js
@@ -13,24 +13,35 @@ module.exports = function (config) {
       require("@angular-devkit/build-angular/plugins/karma"),
     ],
     client: {
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
-    coverageReporter: {
-      type: "html",
-      dir: "coverage/",
+    jasmineHtmlReporter: {
+      suppressAll: true, // removes the duplicated traces
     },
-    reporters: ["progress", "kjhtml", "coverage"],
+    coverageReporter: {
+      dir: require("path").join(__dirname, "./coverage/ping-pong-scoring-app"),
+      subdir: ".",
+      reporters: [{ type: "html" }, { type: "text-summary" }],
+    },
+    reporters: ["progress", "kjhtml"],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ["ChromeDebugging"],
     singleRun: false,
+    restartOnFileChange: true,
     customLaunchers: {
       ChromeDebugging: {
         base: "Chrome",
         flags: ["--remote-debugging-port=9222"],
       },
     },
-  });
+});
 };
